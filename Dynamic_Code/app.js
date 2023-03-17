3edbc4ae49d43f6bcad70c3701d5ddb04b6488e6
Seun0001 <oluwaseunafolabi941@gmail.com>
2023-03-17 13:44:46 +0000

    Changes finally done

--- Dynamic_Code/app.js
+++ Dynamic_Code/app.js
@@ -1,1 +1,36 @@
-!
+const reset1Button = document.querySelector("#reset1");
+const reset2Button = document.querySelector("#reset2");
+
+const dogCart = document.querySelector(".Dog_cart");
+const underline1 = document.querySelector(".underline_1");
+
+reset1Button.addEventListener("click", () => {
+    dogCart.remove();
+    underline1.remove();
+});
+
+const nutroCart = document.querySelector(".Nutro_cart");
+const underline2 = document.querySelector(".underline_2");
+
+reset2Button.addEventListener("click", () => {
+    nutroCart.remove();
+    underline2.remove();
+});
+
+const checkoutBtn = document.getElementById("checkout-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+checkoutBtn.onclick = function () {
+    modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+};
