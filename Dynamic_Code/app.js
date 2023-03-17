9054de72f462fe550fde4c9acb7ad94e5bcba410
Seun0001 <oluwaseunafolabi941@gmail.com>
2023-03-17 16:50:39 +0000

    New Changes Done

--- Dynamic_Code/app.js
+++ Dynamic_Code/app.js
@@ -1,3 +1,4 @@
+// Reset Button For .Dingo_cart
 const reset1Button = document.querySelector("#reset1");
 const reset2Button = document.querySelector("#reset2");
 
@@ -9,6 +10,7 @@ reset1Button.addEventListener("click", () => {
     underline1.remove();
 });
 
+// Reset Button For .Nutro_cart
 const nutroCart = document.querySelector(".Nutro_cart");
 const underline2 = document.querySelector(".underline_2");
 
@@ -34,3 +36,87 @@ window.onclick = function (event) {
     modal.style.display = "none";
     }
 };
+
+// Increment/Decrement amount of items for .Dingo_cart
+const amountInput1 = document.getElementById("amount-input_1");
+const price1 = 12.99; // the price of item 1
+const subTotalPrice1 = document.querySelector("#sub-total_1");
+
+amountInput1.addEventListener("input", function () {
+    const amount1 = parseInt(amountInput1.value);
+    const newSubTotalPrice1 = amount1 * price1;
+    subTotalPrice1.textContent = newSubTotalPrice1.toFixed(2);
+    });
+
+// Increment/Decrement amount of items for .Nutro_cart
+const amountInput2 = document.getElementById("amount-input_2");
+const price2 = 9.99; // the price of item 2
+const subTotalPrice2 = document.querySelector("#sub-total_2");
+
+amountInput2.addEventListener("input", function () {
+    const amount2 = parseInt(amountInput2.value);
+    const newSubTotalPrice2 = amount2 * price2;
+    subTotalPrice2.textContent = newSubTotalPrice2.toFixed(2);
+    });
+
+
+
+
+
+
+// Shows The Total Amount Of Items Ordered In Modal
+const checkoutButton = document.getElementById("checkout-btn");
+checkoutButton.addEventListener("click", function () {
+    const subTotal =
+    parseFloat(subTotalPrice1.textContent) +
+    parseFloat(subTotalPrice2.textContent);
+  const tax = subTotal * 0.05;
+    const shippingCost = 15;
+    const grandTotal = subTotal + tax + shippingCost;
+
+    const modal = document.getElementById("modal");
+    const modalContent = modal.querySelector(".modal-content");
+    modalContent.innerHTML =
+    "<span class='close'>&times;</span><p><em><b>Your checkout information here :</b></em><br><b>Congratulations!!!</b><br>Your finally submitting your request to us.<br>Have a nice time ahead.</p><img src='../images/ballon.jpg' alt='congratulation image'><p>Subtotal: $" +
+    subTotal.toFixed(2) +
+    "</p><p>Tax: $" +
+    tax.toFixed(2) +
+    "</p><p>Shipping: $" +
+    shippingCost.toFixed(2) +
+    "</p><p>Grand Total: $" +
+    grandTotal.toFixed(2) +
+    "</p>";
+
+    modal.style.display = "block";
+
+    const closeButton = modal.querySelector(".close");
+    closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+    });
+});
+
+
+
+
+
+//  subtotal, shipping, and grand total prices in the HTML based on the form inputs in the .Dog_cart and .Nutro_cart elements
+
+const totalPrice = document.getElementsById("Total_price");
+totalPrice.addEventListener("click", function () {
+    const subTotal =
+    parseFloat(subTotalPrice1.textContent) +
+    parseFloat(subTotalPrice2.textContent);
+    const tax = subTotal * 0.05;
+    const shippingCost = 15;
+    const grandTotal = subTotal + tax + shippingCost;
+
+    totalPrice.style.display = "block";
+
+    closeButton.addEventListener("click", function () {
+    totalPrice.style.display = "none";
+    });
+});
+
+
+
+// Next last things In My Code
--- README.md
+++ README.md
@@ -7,4 +7,6 @@
 - That is all done.
 - Author: Afolabi John Oluwaseun
 
++ This is an HTML document that defines the structure and content of a shopping cart webpage. The document includes a header with a title and navigation links, and a main section that displays the items in the shopping cart, including their images, descriptions, prices, quantities, and subtotals. It also displays the subtotal, tax, shipping, and grand total of the order, as well as a checkout button that triggers a modal window with a congratulatory message. The document also includes a CSS stylesheet and a JavaScript file to add interactivity to the page.
+
 ```
